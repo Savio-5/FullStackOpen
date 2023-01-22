c864ddbc1a3d4a7c78f33a31394ba4f4e11784d8
Savio Silveira <s3.silveira@gmail.com>
2023-01-22 18:08:27 +0530

    1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,7 +5,21 @@ const StatisticHeader = () => <h1>statistics</h1>
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
-const Statistic = ({text,type}) => <p>{text} {type}</p>
+const StatisticLine = ({text,type}) => <p>{text} {type}</p>
+
+const Statistics = ({good,neutral,bad,all}) => {
+  return(
+    <>
+      <StatisticHeader />
+      <StatisticLine text='good' type={good}/>
+      <StatisticLine text='neutral' type={neutral}/>
+      <StatisticLine text='bad' type={bad}/>
+      <StatisticLine text='all' type={all}/>
+      <StatisticLine text='average' type={(good - bad) / all}/>
+      <StatisticLine text='positive' type={((good / all) * 100) + ' %' }/>
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -20,13 +34,7 @@ const App = () => {
       <Button handleClick={() => setGood(good+1)} text='good'/>
       <Button handleClick={() => setNeutral(neutral+1)} text='neutral'/>
       <Button handleClick={() => setBad(bad+1)} text='bad'/>
-      <StatisticHeader />
-      <Statistic text='good' type={good}/>
-      <Statistic text='neutral' type={neutral}/>
-      <Statistic text='bad' type={bad}/>
-      <Statistic text='all' type={all}/>
-      <Statistic text='average' type={(good - bad) / all}/>
-      <Statistic text='positive' type={((good / all) * 100) + ' %' }/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
     </div>
   )
 }
