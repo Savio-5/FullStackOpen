cfe9a4e35b9a9e585e334bb2cd1b6a420abadbd5
Savio-5 <s3.silveira@gmail.com>
2023-02-06 17:58:10 +0530

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   function handleEvent(event) {
     event.preventDefault()
@@ -12,31 +13,36 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     } else {
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(personObject))
     }
     setNewName('')
+    setNewNumber('')
   }
 
   function handleChangeName(event) {
     setNewName(event.target.value)
   }
 
+  function handleChangeNumber(event) {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleEvent}>
-        <div>
-          name: <input type='text' value={newName} onChange={handleChangeName}/>
-        </div>
+        <div>name: <input type='text' value={newName} onChange={handleChangeName}/></div>
+        <div>number: <input type='text' value={newNumber} onChange={handleChangeNumber}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )
