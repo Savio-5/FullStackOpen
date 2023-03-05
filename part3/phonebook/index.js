4445b509c3e3f5d4488a40cff094735766ffe78f
Savio Silveira <s3.silveira@gmail.com>
2023-03-05 13:22:30 +0530

    3.6: Phonebook backend step6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -52,15 +52,17 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
+
     if (!body.name) {
-        return res.status(400).json({error: 'name missing'})
+        return res.status(404).json({error: 'name missing'})
     }
     if (!body.number) {
-        return res.status(400).json({error: 'number missing'})
+        return res.status(404).json({error: 'number missing'})
     }
     if (persons.find(person => person.name === body.name)) {
-        return res.status(400).json({error: 'name must be unique'})
+        return res.status(406).json({error: 'name must be unique'})
     }
+
     const person = {
         id: Math.floor(Math.random() * 1000000),
         name: body.name,
